b289cf5428f3a072dc8907f6e87e8c89d7373f75
Martijn <martijn.brug@gmail.com>
2026-02-02 10:07:50 +0100

    feat: ui updates

---
+++ src/locales.ts
@@ -0,0 +1,10 @@
+export const locales = {
+  en: {
+    welcome: "Hello {username}",
+    heroTitle: "New arrivals are here"
+  },
+  nl: {
+    welcome: "Hallo {username}",
+    heroTitle: "Nieuwe aankomsten zijn hier"
+  }
+}
