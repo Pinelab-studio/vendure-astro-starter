10a06d3723065bac3a9d12f17e887a07d19b3296
Martijn <martijn.brug@gmail.com>
2026-02-02 10:03:51 +0100

    feat: ui updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [],
+  integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
   },
